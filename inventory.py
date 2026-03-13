1cfc119721ba5e7f090b5fd93b5d7f8514cb0fd0
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-13 18:58:37 -0500

    restructuring of validations

--- inventory.py
+++ inventory.py
@@ -1,46 +1,43 @@
-
 #validations
 def sale():
     # name validation
     counter_name=1
     while counter_name ==1:
-        try:
             name = str(input("enter product name: "))
             if not name.isalpha():
                 print("Symbols and Numbers are not allowed")
                 continue
-            break
-        
-        except ValueError:
-            print("Enter a valid value")
-            continue
             
-    counter_name +=2
+            counter_name +=2
     
     # price validation
     counter_price=1
     while counter_price ==1:        
         try:
             price = float(input("enter product price: "))
+            if price <=0:
+                print("ERRO: Enter a valid value")
+                continue
         
             break
         except ValueError:
             print("Enter a valid value")
             continue
         
-    counter_price +=2    
-       
+
     # quantity validation
     counter_quantity=1
     while counter_quantity ==1:  
         try:
             quantity = int(input("enter product quantity: "))
+            if quantity <=0:
+                 print("ERRO: Enter a valid value")
+                 continue
             break
         except ValueError:
             print("Enter a valid value")
             continue
-        
-    counter_quantity +=2
+
     
     sales ={ 
         "product" : name,
@@ -51,3 +48,4 @@ def sale():
     
     return sales
         
+sale()

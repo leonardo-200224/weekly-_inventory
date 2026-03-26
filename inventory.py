f6c553aab3e62ee9bb04308113cc257ec45a5409
leonardo-200224 <ayalaperezleonardo24@gmail.com>
2026-03-26 15:56:46 -0500

    delete_update_search

--- inventory.py
+++ inventory.py
@@ -53,3 +53,107 @@ def sale():
     }
     # the created dictionary is returned
     return sales
+
+
+# buscar producto ingresado por el usuario
+def buscar_producto(inventario):
+    
+    # name validation
+    counter_name=1
+    while counter_name ==1:
+            # Request user data
+            name = str(input("enter product name: ")).strip()
+            if not name.isalpha():
+                print("Symbols and Numbers are not allowed")
+                continue
+            
+            counter_name +=2
+            
+    for dictionary in inventario:
+        if dictionary["product"] == name:
+            print(f"product: {dictionary["product"]} | " 
+                f"precio: {dictionary["price"]}  |  "
+                f"quantity: {dictionary["quantity"]}\n")    
+            
+    return None    
+
+def actualizar_producto(inventario):
+ 
+ # name validation
+    counter_name=1
+    while counter_name ==1:
+            # Request user data
+            name_update = str(input("Enter product name to update : ")).strip()
+            if not name_update.isalpha():
+                print("Symbols and Numbers are not allowed")
+                continue
+            
+            counter_name +=2
+            
+    # mostrar producto
+    for dictionary in inventario:
+        cont=1
+        if dictionary["product"] == name_update:
+                cont=2
+                #menu creation and validation
+                try:
+                    print(f"\n que valor quiere editar del producto: {name_update}")
+                    number= int(input(
+                        "1-price\n"
+                        "2-quantity\n"
+                    ))
+                    
+                    #Conditional statements validating the user's input
+                    if number<=0:
+                        print("You must enter a valid product (1-4)")
+                        continue
+                    
+                    # editar precio
+                    if number ==1:
+                        
+                        # price validation
+                        counter_price=1
+                        while counter_price ==1:        
+                            try:
+                                # Request user data
+                                price_new = float(input("enter new product price: "))
+                                if price_new <=0:
+                                    print("ERRO: Enter a valid value")
+                                    continue
+                            
+                                break
+                            except ValueError:
+                                print("Enter a valid value")
+                                continue
+                        dictionary["price"] = price_new
+    
+                    
+                    # editar cantidad
+                    elif number ==2:
+                        
+                        # quantity validation
+                        counter_quantity=1
+                        while counter_quantity ==1:  
+                            try:
+                                # Request user data
+                                quantity_new = int(input("enter new product quantity: "))
+                                if quantity_new <=0:
+                                    print("ERRO: Enter a valid value")
+                                    continue
+                                break
+                            except ValueError:
+                                print("Enter a valid value")
+                                continue
+                        dictionary["quantity"] = quantity_new
+                
+                    else:
+                        print("You must enter a valid value (1-2)") 
+                        continue  
+                #The error is detected and an error message is displayed.
+                except ValueError:
+                    print("the value must be numeric (1-2)")
+                    continue
+                break
+    
+    if cont == 1:
+        print("Producto no encontrado")
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from inventory import sale
 from summary import summary
 from Calculate import calculate
+from inventory import buscar_producto
+from inventory import actualizar_producto
 
 #list that stores the dictionaries
 all_sales=[]
@@ -15,8 +17,11 @@ while cont ==1:
         number= int(input(
             "1-Add product\n"
             "2-Show inventory\n"
-            "3-calculate statistics\n"
-            "4-go out\n"
+            "3-buscar producto\n"
+            "4-actualizar producto\n"
+            "5-eliminar producto\n"
+            "6-calculate statistics\n"
+            "7-go out\n"
             "Enter product number (1-4): "
         ))
         
@@ -24,20 +29,32 @@ while cont ==1:
         if number<=0:
             print("You must enter a valid product (1-4)")
             continue
-        
+        # agregar producto
         if number ==1:
             all_sales.append(sale())
             continue
-            
+        
+        # mostrar inventario resumen
         elif number ==2:
             summary(all_sales)
             continue
         
+        # buscar producto
         elif number ==3:
+            buscar_producto(all_sales)
+            continue
+        
+        # actualizar producto
+        elif number ==4:
+            actualizar_producto(all_sales)
+            continue
+        
+        # calculo de estadistica
+        elif number ==6:
             calculate(all_sales)
             continue
         
-        elif number ==4:
+        elif number ==7:
             cont =2
         else:
             print("You must enter a valid value (1-4)") 
